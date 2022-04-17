1de20f0cd346ea8776f7b597d776df93cd7aaf92
root <root@LAPTOP-HPHIU1FQ.localdomain>
2022-04-17 11:52:24 -0500

    adding, deleting and posting requests

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,5 +1,5 @@
 ANIMALS = [
-    {
+     {
         "id": 1,
         "name": "Snickers",
         "species": "Dog",
@@ -8,20 +8,64 @@ ANIMALS = [
     },
     {
         "id": 2,
-        "name": "Gypsy",
+        "name": "Brixton",
         "species": "Dog",
-        "locationId": 1,
-        "customerId": 2
+        "locationId": 2,
+        "customerId": 1
     },
     {
         "id": 3,
         "name": "Blue",
         "species": "Cat",
-        "locationId": 2,
-        "customerId": 1
+        "locationId": 3,
+        "customerId": 5
     }
 ]
+
+def create_animal(animal):
+    
+    max_id = ANIMALS[-1]["id"]
+    
+    new_id = max_id + 1
+    
+    animal["id"] = new_id
+    
+    ANIMALS.append(animal)
+    
+    return animal
+
+def delete_animal(id):
+    # Initial -1 value for animal index, in case one isn't found
+    animal_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Store the current index.
+            animal_index = index
+
+    # If the animal was found, use pop(int) to remove it from list
+    if animal_index >= 0:
+        ANIMALS.pop(animal_index)
 
 
 def get_all_animals():
     return ANIMALS
+
+#Function with a single parameter
+def get_single_animal(id):
+    # Variable to hold the found animal, if it exits
+    requested_animal = None
+    # Iterate the ANIMALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for animal in ANIMALS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if animal["id"] == id:
+            requested_animal = animal
+            
+    return requested_animal
+
+# print(ANIMALS[1]['id'], ANIMALS[1]['customerId'])
+        
---
+++ views/locations_requests.py
@@ -0,0 +1,58 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville Kennels",
+        "address": "100 Succeeding lane"
+    },
+    {
+        "id": 2,
+        "name": "Boro Kennels",
+        "address": "100 Succeeding lane"
+    },
+    {
+        "id": 3,
+        "name": "Chattanooga Kennels",
+        "address": "100 Succeeding lane"
+    }
+]
+
+def create_location(location):
+    
+    max_id = LOCATIONS[-1]["id"]
+    
+    new_id = max_id + 1
+    
+    location["id"] = new_id
+    
+    LOCATIONS.append(location)
+    
+    return location
+
+def delete_location(id):
+    
+    location_index = -1
+    
+    for index, location in enumerate(LOCATIONS):
+        if location["id"] == id:
+            
+            location_index = index
+            
+    if location_index >= 0:
+        LOCATIONS.pop(location_index)
+
+
+    
+    
+
+def get_all_locations():
+    return LOCATIONS
+
+def get_single_location(id):
+    
+    requested_location = None
+    
+    for location in LOCATIONS:
+        if location["id"] == id:
+            requested_location = location
+            
+    return requested_location
